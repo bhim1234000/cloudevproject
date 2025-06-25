59ada154bb765c977bf6cea2ab1b247ca2f126c1
bhim1234000 <waghmarebhim612@gmail.com>
2025-06-25 23:27:43 +0530

    adding files app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# Simple Python program
+
+# Print a welcome message
+print("Hello, World!")
+
+# Ask for user input
+name = input("What is your name? ")
+
+# Greet the user
+print("Nice to meet you, " + name + "!")
+
